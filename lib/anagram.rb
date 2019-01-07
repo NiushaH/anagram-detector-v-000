915dc4cf169d45bf934e56a612d173e21add6e05
Niusha H <niusha.hadziomerovic@gmail.com>
2019-01-07 08:05:38 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,11 +1,6 @@
-  def match(find_anagram_in_this_string) 
-    puts "Must find #{self.word_to_analyze} inside the following string: #{find_anagram_in_this_string}"
-    find_anagram_in_this_string.find_all do |word| 
       if word.split("").sort == self.word_to_analyze.split("").sort 
         word 
       end
-    end 
-  end
 
 
 # listen = Anagram.new("listen")
@@ -23,24 +18,24 @@ class Anagram
     @word_to_analyze = self.new 
   end
   
+  def match(does_this_word_have_an_anagram_here)
     #WRITE A LOOP --> break down all letters in the word and see if they exactly match the letters of a different word(an anagram)
-    
     #check if both words have same number of letters
-    
-    if word.count = anagram_instance.count
-      @does_this_word_have_an_anagram_here.each do |does_this_word_have_an_anagram_here|
-      
-        #split the words into array of letters  
-        array_of_words_letters = word.split("").sort
-        array_of_instance_letters = instance.split("").sort
-      
-      #check to see if words match  
-      if array_of_words_letters.match(%w(possible anagram words)) == array_of_instance_letters.sort
-      return array of all matching words
-    else 
-      return empty_array
+
+    does_this_word_have_an_anagram_here.each do |word|    
+      if word.count = anagram_instance.count
+
+          #split the words into array of letters  
+          array_of_words_letters = word.split("").sort
+          array_of_instance_letters = instance.split("").sort
+        
+        #check to see if words match  
+        if array_of_words_letters.match(%w(possible anagram words)) == array_of_instance_letters.sort
+        return array of all matching words
+      else 
+        return empty_array
+        end
       end
-    end
     #WRITE CONDITIONAL CODE --> return an array of all matching words, if there are any, OR if no match, return empty array
   
   end
